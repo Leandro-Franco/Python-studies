5ffa32b9aaaa4824fe786a93b49fa6340154c330
Logistis <leandrofigueiredofranco@gmail.com.br>
2024-01-18 01:54:13 -0300

    Introdução a funções fundamentais

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
-// Código abaixo
+####
+# notas = [6,7,8,9,10]
+# b10 = []
+
+# for nota in notas:
+#     b10.append(nota * 10)
+
+# b10
+####
+n=5
+
+def factorial(n):
+    last, next = 1, 1
+    while next <= n:
+        last, next = next*last, next + 1
+    return last
+
+
+print(factorial(5), n)
+####
